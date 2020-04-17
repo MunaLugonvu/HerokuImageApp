3382270ae9085163b9022db2a4c0cd4989d86537
muna <munalugonvu@gmail.com>
2020-04-17 19:33:24 +0300

    animated progress bar

--- src/components/progress.js
+++ src/components/progress.js
@@ -2,10 +2,10 @@ import React from "react";
 
 
  const Progress = ({ percentage }) => (
-  <div className="progress mt-0 mb-3">
+  <div className="progress">
 
      <div
-      className="progress-bar progress-bar-striped bg-success"
+      className="progress-bar progress-bar-striped progress-bar-animated"
       role="progressbar"
       style={{ width: `${percentage}%` }}
     >
