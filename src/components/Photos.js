f8dbe93c2ebdad3aad0f8d31c331596a69dd6e01
muna <munalugonvu@gmail.com>
2020-06-09 15:20:24 +0300

    use of hooks

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,50 +1,46 @@
-import React, { Component } from 'react'
-import firebase from "../firebase"
-export default class Photos extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      photos:[]
-    }
-  }
+import React, { useEffect, useState } from 'react';
+import firebase from '../firebase';
+export default function Photos() {
+  const [photos, setPhotos] = useState([]);
 
-  showImage(){
-    let images=[]
-    firebase.firestore().collection(localStorage.getItem("user_id")).get().then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-         
-
-          let imageData={
-            url:doc.data().url,
-            created:doc.data().added
-          }
-          images.push(imageData)
-
+  async function showImages() {
+    let images = [];
+     await firebase
+      .firestore()
+      .collection(localStorage.getItem('user_id'))
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          let imageData = {
+            url: doc.data().url,
+            created: doc.data().added,
+          };
+          images.push(imageData);
+        });
       });
-  });
-  this.setState({ photos: images });
+    setPhotos(images);
   }
-  
-  componentDidMount(){
- this.showImage();
-  }
-  render() {
-    const items=this.state.photos;
 
-    return (
-    
-      <div className="container page-top">
-        <div className='row'>
-        
-      {items.map((i, index)=>(
-      <div className="col-lg-3 col-md-4 col-xs-6 thumb">
-      <img className="img-thumbnail" src={i.url} key={index} alt="Missing"/>
+  useEffect(() => {
+    showImages();
+  }, []);
+
+ const photo = photos;
+
+  return (
+    <div className="container page-top">
+      <div className="row">
+        {photo.map((i, index) => (
+          <div className="col-lg-3 col-md-4 col-xs-6 thumb">
+            <img
+              className="img-thumbnail"
+              src={i.url}
+              key={index}
+              alt="Missing"
+            />
+          </div>
+        ))}
       </div>
-      ))}
-      </div>
-      </div>
-      
-      
-    )
-  }
+    </div>
+  );
 }
